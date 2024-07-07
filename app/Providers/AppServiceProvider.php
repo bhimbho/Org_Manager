1280e730c557aae43208fa5db2623103cf7efbee
bhimbho <advancoplanet@gmail.com>
2024-07-07 05:49:53 +0100

    fix keys issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Passport::personalAccessTokensExpireIn(now()->addDay());
-        Passport::loadKeysFrom(__DIR__.'/secrets/oauth');
+//        Passport::loadKeysFrom(__DIR__.'/secrets/oauth');
     }
 }
